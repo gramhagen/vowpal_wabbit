0d763f395077c3385a9947adc60fb8f25dbb0f91
Scott Graham <5720537+gramhagen@users.noreply.github.com>
2019-09-09 10:04:17 -0400

    fix for non 64bit os

--- .scripts/linux/build-static-java.sh
+++ .scripts/linux/build-static-java.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
+GCC=`which gcc`
+GPP=`which g++`
+
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_DIR=$SCRIPT_DIR/../../
 cd $REPO_DIR
@@ -11,7 +14,7 @@ cd build
 # Boost unit tests don't like the static linking
 # /usr/local/bin/gcc + g++ is 9.2.0 version
 cmake .. -DCMAKE_BUILD_TYPE=Release -DWARNINGS=Off -DBUILD_JAVA=On -DBUILD_DOCS=Off \
- -DBUILD_PYTHON=Off -DSTATIC_LINK_VW_JAVA=On -DCMAKE_C_COMPILER=/usr/local/bin/gcc -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ \
+ -DBUILD_PYTHON=Off -DSTATIC_LINK_VW_JAVA=On -DCMAKE_C_COMPILER=$GCC -DCMAKE_CXX_COMPILER=$GPP \
  -DBUILD_TESTS=Off
 NUM_PROCESSORS=$(cat nprocs.txt)
 make all -j ${NUM_PROCESSORS}
